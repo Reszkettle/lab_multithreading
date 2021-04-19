48dae76eabf3874f6d8e0b45dc33563a03fabb68
Reszkettle <reszkettle@gmail.com>
2021-04-19 18:56:11 +0200

    Changed logic inside BetterRadar to be mockable

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,12 +1,20 @@
 package edu.iis.mto.multithread;
 
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BetterRadar {
 
     private final ExecutorService executor;
     private final int rocketsCount;
     private final PatriotBattery battery;
+
+    public BetterRadar(int rocketsCount, PatriotBattery battery) {
+        this.rocketsCount = rocketsCount;
+        this.battery = battery;
+        this.executor = Executors.newSingleThreadExecutor();
+    }
 
     public BetterRadar(ExecutorService executor, int rocketsCount, PatriotBattery battery) {
         this.executor = executor;
